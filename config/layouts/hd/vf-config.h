24607f09e4197a05241d089465f8fa0b7f30fe08
munkca <137710061+munkca@users.noreply.github.com>
2025-09-28 09:55:58 +0800

    Update vf-config.h

--- config/layouts/hd/vf-config.h
+++ config/layouts/hd/vf-config.h
@@ -64,7 +64,7 @@
 #define DOT_LB _______        _______         _______         &b_wd           _______
 #define DOT_LH                                                _______         _______
 
-#define DOT_RT &kp DLLR       &kp DOT         &t_dotcom       &t_dotedu       &t_dotorg
+#define DOT_RT &kp DLLR       &kp DOT         _______         _______         _______
 #define DOT_RM &caps_word     _______         _______         _______         _______
 #define DOT_RB _______        _______         _______         _______         _______
 #define DOT_RH _______        _______
